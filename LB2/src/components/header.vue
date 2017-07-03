204437489e39c2bf515f6a7ecb7c38f5ce61cc88
manojkumar3692 <manoj.officialmail@gmail.com>
2017-07-03 17:26:38 +0530

    bug

--- LB2/src/components/header.vue
+++ LB2/src/components/header.vue
@@ -1,27 +1,33 @@
 <template>
 <section class="header row">
 <div class="col-lg-6 col-md-6">
-  <h1>Little Brahma Phase 2</h1>
+  <h1>Vuejs 2 Authentication</h1>
 </div>
 <div class="col-lg-6 col-md-6">
-  <h2>Logout</h2>
+  <h2 v-on:click="logout()" style="cursor:pointer;">Logout</h2>
 </div>
   
 </section>
 </template>
 
 <script>
-  
+  import VueRouter from 'vue-router';
   export default {
     data() {
       return {
         message:'Header'
       }
+    },
+    methods: {
+        logout:function() {
+           var app = this;
+         localStorage.removeItem("lbUser");
+         app.$router.push({ name: 'login'});
+        }
     }
   }
 
 </script>
 
 <style>
-
 </style>
